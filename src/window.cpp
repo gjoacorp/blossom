ca2b367007f32a4e67be00d7d9937e8728ad5d4f
eugenekingfish <aojg@protonmail.ch>
2025-05-26 07:59:24 +0100

    minor formatting changes

--- src/window.cpp
+++ src/window.cpp
@@ -1,25 +1,35 @@
 #include "../headers/window.h"
 #include <iostream>
 
-
-window::window(const int& size_x, const int& size_y, const char* title) {
-   if (!glfwInit()) {
+window::window(const int& size_x, const int& size_y, const char* title) 
+{
+   if ( !glfwInit() ) 
+   {
       std::cout << "Failed to initialise GLFW." << std::endl;
       exit(EXIT_FAILURE); 
    }
+
    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
    this->window_ptr = glfwCreateWindow(size_x, size_y, title, NULL, NULL);
 }
 
 
-void window::init() {
+void window::init() 
+{
    glfwMakeContextCurrent(window_ptr);
-   if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
+
+   if ( glewInit() != GLEW_OK ) 
+   { 
+      exit(EXIT_FAILURE); 
+   }
+
    glfwSwapInterval(1); // sets the color buffer swap interval (i.e. vsync)
 }
 
-void window::destroy() const {
+void window::destroy() const 
+{
    glfwDestroyWindow(window_ptr);
    glfwTerminate();
 }
