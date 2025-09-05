9f1ea47fd4a09b1993ec56bf1e1af573119acdd9
eugenekingfish <aojg@protonmail.ch>
2025-09-05 19:46:39 +0100

    replaced triangles example with triangle example

---
+++ examples/triangle.cpp
@@ -0,0 +1,39 @@
+#include "../headers/shader.h"
+#include "../headers/mesh.h"
+#include "../headers/window.h"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+const unsigned int WINDOW_WIDTH = 1920;
+const unsigned int WINDOW_HEIGHT = 1080;
+const GLfloat clear_color[] = { 0.0f, 0.0f, 0.0f, 0.0f};
+
+int main(void) 
+{
+  window w(1920, 1080, "Triangle Example");
+  w.enter_fullscreen();
+  w.init();
+
+  std::vector<glm::vec3> vertices =
+  {
+    {0.0f, 0.577f, 0.0f},
+    {-0.5f, -0.289f, 0.0f},
+    {0.5f, -0.289f, 0.0f}
+  };
+
+  shader triangle_shader("shaders/triangles.frag", "shaders/triangles.vert");
+  GLuint shader_program = triangle_shader.init();
+
+  mesh triangle(vertices, shader_program);
+  triangle.init();
+
+  while ( !glfwWindowShouldClose(w.window_ptr) )
+  {
+    glClearBufferfv(GL_COLOR, 0, clear_color);
+    triangle.draw();
+    glfwSwapBuffers(w.window_ptr);
+    glfwPollEvents();
+  }
+
+  w.destroy();
+}
