f6db82c4fe18a59ca8fc08d95d78e31dca22cb6b
eugenekingfish <aojg@protonmail.ch>
2025-09-06 20:36:22 +0100

    added IBO support to mesh class

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -3,11 +3,24 @@
 void mesh::draw() const
 {
   glUseProgram(shader_program_);
-  glDrawArrays(GL_TRIANGLES, 0, (GLsizei)vertices.size());
+
+  if (indices.size() > 0) 
+  { 
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0); 
+  }
+  else 
+  { 
+    glDrawArrays(GL_TRIANGLES, 0, (GLsizei)vertices.size()); 
+  }
 }
 
 mesh::mesh(const std::vector<glm::vec3>& vertices, GLuint shader_program) : 
   vertices(vertices), 
+  shader_program_(shader_program) {}
+
+mesh::mesh(const std::vector<glm::vec3>& vertices, const std::vector<GLuint>& indices, GLuint shader_program) :
+  vertices(vertices),
+  indices(indices),
   shader_program_(shader_program) {}
 
 mesh::~mesh()
@@ -17,17 +30,30 @@ mesh::~mesh()
 
   if (vbo_ != 0)
     glDeleteBuffers(1, &vbo_);
+
+  if (ibo_ != 0)
+    glDeleteBuffers(1, &ibo_);
 }
 
 void mesh::init()
 {
-  glCreateVertexArrays(1, &vao_);
-  glBindVertexArray(vao_);
+  if (glfwGetCurrentContext())
+  {
+    glCreateVertexArrays(1, &vao_);
+    glBindVertexArray(vao_);
 
-  glCreateBuffers(1, &vbo_);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo_);
-  glNamedBufferStorage(vbo_, vertices.size() * sizeof(glm::vec3), &vertices[0], 0);
+    glCreateBuffers(1, &vbo_);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+    glNamedBufferStorage(vbo_, vertices.size() * sizeof(glm::vec3), &vertices[0], 0);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)0);
-  glEnableVertexAttribArray(0);
+    if (indices.size() > 0)
+    {
+      glCreateBuffers(1, &ibo_);
+      glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo_);
+      glNamedBufferStorage(ibo_, indices.size() * sizeof(GLuint), &indices[0], 0);
+    }
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)0);
+    glEnableVertexAttribArray(0);
+  }
 }
