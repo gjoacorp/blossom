0bc870f4a598c7d5aef9824613eb8f63f804d2f4
eugenekingfish <aojg@protonmail.ch>
2025-09-08 22:50:59 +0100

    added a rudimentary rectangle class (header only)

---
+++ headers/rectangle.h
@@ -0,0 +1,34 @@
+#ifndef BLOSSOM_RECTANGLE_H
+#define BLOSSOM_RECTANGLE_H
+
+#include "mesh.h"
+#include "shader.h"
+
+namespace blossom
+{
+  class rectangle : public mesh
+  {
+    public:
+      rectangle(GLuint shader_program)
+      {
+        vertices_ = 
+        {
+          glm::vec3{ 0.5f,  0.5f, 0.0f}, // top-right
+          glm::vec3{-0.5f,  0.5f, 0.0f}, // top-left
+          glm::vec3{-0.5f, -0.5f, 0.0f}, // bottom-left
+          glm::vec3{ 0.5f, -0.5f, 0.0f}  // bottom-right
+        };
+
+        indices_ = 
+        {
+          0, 1, 2, 
+          0, 2, 3  
+        };
+
+        shader_program_ = shader_program;
+        init_();
+      }
+  };
+}
+
+#endif
