d037acf94e701315bb400186e6e09b5685a699c3
eugenekingfish <aojg@protonmail.ch>
2025-09-09 18:51:57 +0100

    added new rectangle example

---
+++ examples/rectangle.cpp
@@ -0,0 +1,30 @@
+#include "../headers/window.h"
+#include "../headers/rectangle.h"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+const unsigned int WINDOW_WIDTH = 1920;
+const unsigned int WINDOW_HEIGHT = 1080;
+const GLfloat clear_color[] = { 0.0f, 0.0f, 0.0f, 0.0f};
+
+int main(void) 
+{
+  blossom::window w(1920, 1080, "Triangle Example");
+  w.enter_fullscreen();
+  w.init();
+
+  blossom::shader rect_shader {"shaders/rectangle.frag", "shaders/rectangle.vert"};
+  blossom::rectangle rect {rect_shader.program_id};
+
+  while ( !glfwWindowShouldClose(w.window_ptr) )
+  {
+    glClearBufferfv(GL_COLOR, 0, clear_color);
+    rect.draw();
+    glfwSwapBuffers(w.window_ptr);
+    glfwPollEvents();
+  }
+
+  w.destroy();
+}
---
+++ shaders/rectangle.frag
@@ -0,0 +1,8 @@
+#version 450 core
+
+layout (location = 0) out vec4 fColor;
+
+void main() 
+{
+  fColor = vec4(0.7f, 0.3f, 0.1f, 1.0f);
+}
