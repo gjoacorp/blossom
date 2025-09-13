be56214f5085ee54479306af54566d4ecb5f6d58
eugenekingfish <aojg@protonmail.ch>
2025-09-13 15:51:39 +0100

    restructured camera and orthographic_camera classes * camera now inherits from transform, removing the need for orthographic_camera to do so * update_view_matrix() is now virtual instead of pure virtual and defined in camera * minor tweak to the camera constructor * added near and far public attributes to camera

--- headers/camera.h
+++ headers/camera.h
@@ -2,21 +2,35 @@
 #define BLOSSOM_CAMERA_H
 
 #include <glm/glm.hpp>
+#include "transform.h"
+#include <glm/gtc/matrix_transform.hpp>
 
 namespace blossom
 {
-  class camera 
+  class camera : public transform
   {
     public:
       int width;
       int height;
 
+      float near;
+      float far;
+
       glm::mat4 view_matrix = glm::mat4(1.0f);
       glm::mat4 projection_matrix = glm::mat4(1.0f);
 
-      camera(int width, int height) : width(width), height(height) {}
+      camera(const glm::vec3& position, int width, int height) : transform(position), width(width), height(height) {}
 
-      virtual void update_view_matrix() = 0;
+      virtual void update_view_matrix()
+      {
+        glm::mat4 view_matrix = glm::mat4(1.0f);
+        view_matrix = glm::rotate(view_matrix, -rotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
+        view_matrix = glm::rotate(view_matrix, -rotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
+        view_matrix = glm::rotate(view_matrix, -rotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
+        view_matrix = glm::scale(view_matrix, scale); 
+        view_matrix = glm::translate(view_matrix, -position);
+      }
+
       virtual void update_projection_matrix() = 0;
   };
 }
--- headers/orthographic_camera.h
+++ headers/orthographic_camera.h
@@ -1,31 +1,18 @@
 #ifndef BLOSSOM_ORTHOGRAPHIC_CAMERA_H
 #define BLOSSOM_ORTHOGRAPHIC_CAMERA_H
 
-#include <glm/gtc/matrix_transform.hpp>
-#include "transform.h"
 #include "camera.h"
 
 namespace blossom
 {
-  class orthographic_camera : public camera, public transform
+  class orthographic_camera : public camera
   {
     public:
       orthographic_camera(const glm::vec3& position, int width, int height) :
-        transform(position),
-        camera(width, height)
+        camera(position, width, height)
       {
         update_view_matrix();
         update_projection_matrix();
-      }
-
-      void update_view_matrix()
-      {
-        glm::mat4 view_matrix = glm::mat4(1.0f);
-        view_matrix = glm::rotate(view_matrix, -rotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
-        view_matrix = glm::rotate(view_matrix, -rotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
-        view_matrix = glm::rotate(view_matrix, -rotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
-        view_matrix = glm::scale(view_matrix, scale); 
-        view_matrix = glm::translate(view_matrix, -position);
       }
 
       void update_projection_matrix()
