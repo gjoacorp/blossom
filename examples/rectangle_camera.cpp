9c2997f85edc79a67cf0a90fa4381e9b43d49d1a
eugenekingfish <aojg@protonmail.ch>
2025-09-14 15:14:32 +0100

    some minor tweaks to examples/rectangle_camera.cpp

--- examples/rectangle_camera.cpp
+++ examples/rectangle_camera.cpp
@@ -6,28 +6,28 @@
 
 const unsigned int WINDOW_WIDTH = 1920;
 const unsigned int WINDOW_HEIGHT = 1080;
-const GLfloat clear_color[] = { 0.0f, 0.0f, 0.0f, 0.0f};
+const char* WINDOW_TITLE = "Blossom Rectangle Camera Example";
+
+const GLfloat CLEAR_COLOR[] = { 0.0f, 0.0f, 0.0f, 0.0f};
 
 int main() 
 {
-  blossom::window w(1920, 1080, "Blossom Rectangle Camera Example");
-  w.enter_fullscreen();
+  blossom::window window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+  window.enter_fullscreen();
 
   blossom::shader rect_shader {"shaders/default.frag", "shaders/default.vert"};
   blossom::rectangle rect {rect_shader.program_id};
 
   rect.position = {500.0f, 500.0, 0.0f};
   rect.scale = {200.0f, 100.0f, 0.0f};
-  glm::mat4 model_matrix = rect.calc_model_matrix();
-  blossom::orthographic_camera cam {{0.0f, 0.0f, 0.0f}, 1920, 1080};
+  blossom::orthographic_camera camera {{0.0f, 0.0f, 0.0f}, 1920, 1080};
 
-
-  while ( !glfwWindowShouldClose(w.window_ptr) )
+  while ( !glfwWindowShouldClose(window.window_ptr) )
   {
-    glClearBufferfv(GL_COLOR, 0, clear_color);
-    rect.draw(&cam);
-    glfwSwapBuffers(w.window_ptr);
+    glClearBufferfv(GL_COLOR, 0, CLEAR_COLOR);
+    rect.draw(&camera);
+    glfwSwapBuffers(window.window_ptr);
     glfwPollEvents();
   }
-  w.destroy();
+  window.destroy();
 }
