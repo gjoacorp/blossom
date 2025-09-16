ea1149aa5f2abedb59f42c1049e0eef35fd144f8
sonstgenannt <118530645+sonstgenannt@users.noreply.github.com>
2025-09-16 13:07:49 +0100

    added basic view example

---
+++ examples/view.cpp
@@ -0,0 +1,41 @@
+#include "../headers/window.h"
+#include "../headers/perspective_camera.h"
+#include "../headers/sphere.h"
+
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+const char* WINDOW_TITLE = "Blossom Mesh View Example";
+
+int main()
+{
+    blossom::window window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+
+    int width, height;
+    glfwGetFramebufferSize(window.window_ptr, &width, &height);
+    float aspect = float(width) / float(height);
+
+    const glm::vec3 camera_position = { 0.0f, 1.0f, 5.0f };
+    blossom::perspective_camera camera{ camera_position, WINDOW_WIDTH, WINDOW_HEIGHT, 1.0472f, aspect, 100.0f };
+    camera.rotation.x = -45.0f;
+    camera.update_view_matrix();
+
+    blossom::shader shader("shaders/default.frag", "shaders/default.vert");
+
+    blossom::sphere mesh{ shader.program_id };
+
+    while (!glfwWindowShouldClose(window.window_ptr))
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        mesh.draw(&camera);
+        glfwSwapBuffers(window.window_ptr); // swaps the front and back colour buffers
+        glfwPollEvents();
+    }
+
+    window.destroy();
+    exit(EXIT_SUCCESS);
+}
