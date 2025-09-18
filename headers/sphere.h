b167c97a374b2d2dedd647c95db07b4cd4c918da
sonstgenannt <118530645+sonstgenannt@users.noreply.github.com>
2025-09-18 19:46:35 +0100

    fixed documentation tags, added exception for radius choice

--- headers/sphere.h
+++ headers/sphere.h
@@ -7,6 +7,8 @@
 #include <glm/gtc/constants.hpp>
 #include <glm/trigonometric.hpp>
 
+#include <exception>
+
 float pi_f = glm::pi<float>();
 
 namespace blossom
@@ -14,25 +16,35 @@ namespace blossom
 	class sphere : public mesh
 	{
 	public:
-		/// @brief Generates the points of a radius `radius` sphere based on parameters for the vertical and horizontal rings, `sector_count` and `stack_count` respectively, and returns them as `glm::vec3` objects within an array.
+		/** @brief Generates the points of a radius `radius` sphere based on parameters for the vertical and horizontal rings, `sector_count` and `stack_count` respectively, and returns them as `glm::vec3` objects within an lists.
+     * @param[in] radius Radius of sphere in arbitrary units at present.
+     * @param[in] sector_count Number of horizontal rings.
+     * @param[in] stack_count Number of vertical rings.
+     * @return List of vertex positions as `glm::vec3` objects.
+    */
 		static std::vector<glm::vec3> generate_sphere_verts(float radius, std::size_t sector_count, std::size_t stack_count)
 		{
-			std::vector<glm::vec3> verts;	/**< Array of `glm::vec3` vectors for vertices of the sphere*/
-			/// @brief Sets the increment spacing vertices in the loop.
+      if ( radius <= 0.0f )
+      {
+        throw std::invalid_argument ("ERROR : Radius must be a float greater than zero.");
+      }
+
+			std::vector<glm::vec3> verts;	/* Array of `glm::vec3` vectors for vertices of the sphere*/
+			// brief Sets the increment spacing vertices in the loop.
 			float sector_step = 2 * pi_f / sector_count;
 			float stack_step = pi_f / stack_count;
 			
 			for (std::size_t i = 0; i <= stack_count; ++i)
 			{
-				float stack_angle = pi_f / 2 - i * stack_step;	/**< Fix angle for this vertical ring in this loop. */
+				float stack_angle = pi_f / 2 - i * stack_step;	/* Fix angle for this vertical ring in this loop. */
 
-				float z = radius * glm::sin(stack_angle);	/**< Fix height at this angle via projection of radius at stack_angle. */
-				float zrad = radius * glm::cos(stack_angle);	/**< Fix radius at specific height by projection.*/
+				float z = radius * glm::sin(stack_angle);	/* Fix height at this angle via projection of radius at stack_angle. */
+				float zrad = radius * glm::cos(stack_angle);	/* Fix radius at specific height by projection.*/
 
 				for (std::size_t j = 0; j <= sector_count; ++j)
 				{
-					float sector_angle = j * sector_step;	/**< Fix angle of vertex for that horizontal ring.*/
-					/// @brief Project radius at specific `z` height to find `x`, `y` coordinates.
+					float sector_angle = j * sector_step;	/* Fix angle of vertex for that horizontal ring.*/
+					// Project radius at specific `z` height to find `x`, `y` coordinates.
 					float x = zrad * glm::cos(sector_angle);
 					float y = zrad * glm::sin(sector_angle);
 
@@ -42,32 +54,37 @@ namespace blossom
 
 			return verts;
 		}
-		/// @brief Generates the indices (tris) of a sphere based on parameters for the vertical and horizontal rings, `sector_count` and `stack_count` respectively, and returns them as `glm::vec3` objects within an array.
+		/** @brief Generates the indices (tris) of a sphere based on parameters for the vertical and horizontal rings, `sector_count` and `stack_count` respectively, and returns them as `glm::vec3` objects within an array.he vertical and horizontal rings, `sector_count` and `stack_count` respectively, and returns them as `glm::vec3` objects within an array.
+     * @param[in] radius Radius of sphere in arbitrary units at present.
+     * @param[in] sector_count Number of horizontal rings.
+     * @param[in] stack_count Number of vertical rings.
+     * @return List of indices of vertex set describing each tri.
+    */
 		static std::vector<GLuint> generate_sphere_indices(std::size_t sector_count, std::size_t stack_count)
 		{
 
-			std::vector<GLuint> indices; /**< A vector of `GLuint` objects that will hold the indices by way of index.*/
+			std::vector<GLuint> indices; /* A vector of `GLuint` objects that will hold the indices by way of index.*/
 
-			int k1, k2;	// current and next index
+      std::size_t current_stack, next_stack;
 
 			for (std::size_t i = 0; i < stack_count; ++i)
 			{
-				k1 = i * (sector_count + 1);	/**< Index of current stack. */
-				k2 = k1 + (sector_count + 1);	/**< Index of next stack. */
+				current_stack = i * (sector_count + 1);	/* Index of current stack. */
+				next_stack = current_stack + (sector_count + 1);	/* Index of next stack. */
 
-				for (std::size_t j = 0; j < sector_count; ++j, ++k1, ++k2)
+				for (std::size_t j = 0; j < sector_count; ++j, ++current_stack, ++next_stack)
 				{
 					if (i != 0)
 					{
-						indices.push_back(k1);
-						indices.push_back(k2);
-						indices.push_back(k1 + 1);
+						indices.push_back(current_stack);
+						indices.push_back(next_stack);
+						indices.push_back(current_stack + 1);
 					}
 					if (i != (stack_count - 1))
 					{
-						indices.push_back(k1 + 1);
-						indices.push_back(k2);
-						indices.push_back(k2 + 1);
+						indices.push_back(current_stack + 1);
+						indices.push_back(next_stack);
+						indices.push_back(next_stack + 1);
 					}
 				}
 			}
@@ -75,10 +92,12 @@ namespace blossom
 			return indices;
 		}
 
-		/// @brief Constructor for a sphere with the default parameters of 0.5 radius, 32 points in each horizontal ring, of which there are 16.
+		/** @brief Constructor for a sphere with the default parameters of radius 1, 32 points in each horizontal ring, of which there are 16.
+      @param[in] shader_program Shader program used on mesh.
+    */
 		sphere(GLuint shader_program) // unit sphere with default parameters
 		{
-			vertices_ = generate_sphere_verts(0.5, 32, 16);
+			vertices_ = generate_sphere_verts(1.0f, 32, 16);
 
 			indices_ = generate_sphere_indices(32, 16);
 
@@ -86,10 +105,14 @@ namespace blossom
 			init_buffers_();
 			update_uniform_locations_();
 		}
-		/// @brief Constructor for sphere with specific parameters.
-		sphere(GLuint shader_program, float radius, std::size_t sector_count, std::size_t stack_count)  // sphere with specific parameters
+		/** @brief Constructor for sphere of radius 1 with specific parameters.
+     * @param[in] sector_count Number of horizontal rings.
+     * @param[in] stack_count Number of vertical rings.
+     * @param[in] shader_program Shader program used on mesh.
+    */
+		sphere(GLuint shader_program, std::size_t sector_count, std::size_t stack_count)  // sphere with specific parameters
 		{
-			vertices_ = generate_sphere_verts(radius, sector_count, stack_count);
+			vertices_ = generate_sphere_verts(1.0f, sector_count, stack_count);
 
 			indices_ = generate_sphere_indices(sector_count, stack_count);
 
