b3c607af92e19c0dabbfc92f91e37964d9d55920
eugenekingfish <aojg@protonmail.ch>
2025-09-26 04:39:35 +0100

    refactor(mesh_tests.cpp): added test fixture class

--- tests/mesh_test.cpp
+++ tests/mesh_test.cpp
@@ -3,41 +3,53 @@
 #include "../headers/mesh.h"
 #include "../headers/window.h"
 
-// vsp -> vertices, shader program
-TEST(mesh_test, vsp_constructor_with_no_gl_context_throws) 
+class mesh_test : public ::testing::Test 
 {
-  std::vector<glm::vec3> empty_vertices;
-  auto invalid_id = static_cast<GLuint>(0);
-  EXPECT_THROW(blossom::mesh invalid_mesh(empty_vertices, invalid_id), std::runtime_error);
+  protected:
+    std::vector<glm::vec3> empty_vertices_;
+    std::vector<GLuint> empty_indices_;
+    GLuint invalid_id_;
+
+    std::unique_ptr<blossom::window> dummy_window_;
+
+    void SetUp() override 
+    {
+      empty_vertices_.clear();
+      empty_indices_.clear();
+      invalid_id_ = static_cast<GLuint>(0);
+    }
+
+    void create_dummy_window_()
+    {
+      dummy_window_ = std::make_unique<blossom::window>(1, 1, "");
+    }
+};
+
+// vsp -> vertices, shader program
+TEST_F(mesh_test, vsp_constructor_with_no_gl_context_throws) 
+{
+  EXPECT_THROW(blossom::mesh invalid_mesh(empty_vertices_, invalid_id_), std::runtime_error);
 }
 
 // visp -> vertices, indices, shader program
-TEST(mesh_test, visp_constructor_with_no_gl_context_throws)
+TEST_F(mesh_test, visp_constructor_with_no_gl_context_throws)
 {
-  std::vector<glm::vec3> empty_vertices;
-  std::vector<GLuint> empty_indices;
-  auto invalid_id = static_cast<GLuint>(0);
-  EXPECT_THROW(blossom::mesh invalid_mesh(empty_vertices, empty_indices, invalid_id), std::runtime_error);
+  EXPECT_THROW(blossom::mesh invalid_mesh(empty_vertices_, empty_indices_, invalid_id_), std::runtime_error);
 }
 
-TEST(mesh_test, vsp_constructor_with_zero_shader_program_throws)
+TEST_F(mesh_test, vsp_constructor_with_zero_shader_program_throws)
 {
-  blossom::window dummy_window(1, 1, "");
-  std::vector<glm::vec3> empty_vertices;
-  auto invalid_id = static_cast<GLuint>(0);
-  EXPECT_THROW(blossom::mesh invalid_mesh(empty_vertices, invalid_id), std::invalid_argument);
+  create_dummy_window_();
+  EXPECT_THROW(blossom::mesh invalid_mesh(empty_vertices_, invalid_id_), std::invalid_argument);
 }
 
-TEST(mesh_test, visp_constructor_with_zero_shader_program_throws)
+TEST_F(mesh_test, visp_constructor_with_zero_shader_program_throws)
 {
-  blossom::window dummy_window(1, 1, "");
-  std::vector<glm::vec3> empty_vertices;
-  std::vector<GLuint> empty_indices;
-  auto invalid_id = static_cast<GLuint>(0);
-  EXPECT_THROW(blossom::mesh invalid_mesh(empty_vertices, empty_indices, invalid_id), std::invalid_argument);
+  create_dummy_window_();
+  EXPECT_THROW(blossom::mesh invalid_mesh(empty_vertices_, empty_indices_, invalid_id_), std::invalid_argument);
 }
 
-TEST(mesh_test, set_polygon_mode_throws_on_invalid_enum)
+TEST_F(mesh_test, set_polygon_mode_throws_on_invalid_enum)
 {
   blossom::mesh empty_mesh;
   GLenum invalid_enum = GL_TEXTURE_2D;
