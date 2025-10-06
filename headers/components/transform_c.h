e84ac75a8b5aa69f1365cc7a722b192c3993beac
eugenekingfish <aojg@protonmail.ch>
2025-10-06 06:30:26 +0100

    feat: added headers/components - introduced transform component

---
+++ headers/components/transform_c.h
@@ -0,0 +1,15 @@
+#ifndef BLOSSOM_TRANSFORM_C_H
+#define BLOSSOM_TRANSFORM_C_H
+
+#include <glm/glm.hpp>
+
+struct transform_c
+{
+  glm::vec3 position = { 0.0F, 0.0F, 0.0F };
+  glm::vec3 rotation = { 0.0F, 0.0F, 0.0F };
+  glm::vec3 scale    = { 1.0F, 1.0F, 1.0F };
+
+  glm::mat4 transform_matrix = glm::mat4(1.0F);
+};
+
+#endif 
