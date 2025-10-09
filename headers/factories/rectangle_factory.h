c76ef4a4c8b2fa207fec7c697abe404aa1da5ec6
eugenekingfish <aojg@protonmail.ch>
2025-10-09 12:49:39 +0100

    feat: added rectangle_factory

---
+++ headers/factories/rectangle_factory.h
@@ -0,0 +1,37 @@
+#ifndef BLOSSOM_RECTANGLE_FACTORY_H
+#define BLOSSOM_RECTANGLE_FACTORY_H
+
+#include <entt/entt.hpp>
+#include "../factories/mesh_factory.h"
+
+namespace blossom::factory
+{
+  inline auto rectangle(entt::registry& registry, const glm::vec3& position, const glm::vec2& scale, GLuint shader_program) -> entt::entity
+  {
+    const std::vector<glm::vec3> VERTICES = 
+    {
+      glm::vec3{ 1.0F,  1.0F, 0.0F}, 
+      glm::vec3{-1.0F,  1.0F, 0.0F}, 
+      glm::vec3{-1.0F, -1.0F, 0.0F},
+      glm::vec3{ 1.0F, -1.0F, 0.0F} 
+    };
+    
+    const std::vector<GLuint> INDICES = 
+    {
+      0, 1, 2, 
+      0, 2, 3 
+    };
+
+    const glm::vec3 SCALE = {scale.x, scale.y, 1.0F};
+
+    return mesh(registry)
+      .with_position(position)
+      .with_scale(SCALE)
+      .with_vertices(VERTICES)
+      .with_indices(INDICES)
+      .with_shader_program(shader_program)
+      .build();
+  }
+}
+
+#endif
