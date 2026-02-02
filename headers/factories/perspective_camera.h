aa74ee90e9d3e665541fb82580ba5de6c0e3e1e1
eugenekingfish <aojg@protonmail.ch>
2026-02-02 21:56:07 +0000

    refactor: remove raw ptrs from persp cam factory

--- headers/factories/perspective_camera.h
+++ headers/factories/perspective_camera.h
@@ -15,37 +15,37 @@ namespace blossom::factory
         : registry_(registry)
       {
         entity_    =  registry_.create();
-        camera_    = &registry_.emplace<component::perspective_camera>(entity_);
-        transform_ = &registry_.emplace<component::transform>(entity_);
+        registry_.emplace<component::perspective_camera>(entity_);
+        registry_.emplace<component::transform>(entity_);
       }
 
       auto with_width(const uint16_t width) -> perspective_camera&
       {
-        camera_->width = width;
+        registry_.get<component::perspective_camera>(entity_).width = width;
         return *this;
       }
 
       auto with_height(const uint16_t height) -> perspective_camera&
       {
-        camera_->height = height;
+        registry_.get<component::perspective_camera>(entity_).height = height;
         return *this;
       }
 
       auto with_fov_y(float fov_y) -> perspective_camera&
       {
-        camera_->fov_y = fov_y;
+        registry_.get<component::perspective_camera>(entity_).fov_y = fov_y;
         return *this;
       }
 
       auto with_position(const glm::vec3& position) -> perspective_camera&
       {
-        transform_->position = position;
+        registry_.get<component::transform>(entity_).position = position;
         return *this;
       }
 
       auto with_rotation(const glm::vec3& rotation) -> perspective_camera&
       {
-        transform_->rotation = rotation;
+        registry_.get<component::transform>(entity_).rotation = rotation;
         return *this;
       }
 
@@ -60,8 +60,6 @@ namespace blossom::factory
     private:
       entt::registry& registry_;
       entt::entity entity_;
-      component::perspective_camera* camera_;
-      component::transform* transform_;
   };
 }
 
