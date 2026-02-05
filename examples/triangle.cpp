245455f92bd2c4af89ba99f7ed4778beabf283c3
eugenekingfish <aojg@protonmail.ch>
2026-02-05 00:13:54 +0000

    fix: update triangle example - utilise the camera - use the default shader

--- examples/triangle.cpp
+++ examples/triangle.cpp
@@ -2,6 +2,8 @@
 #include "../headers/window.h"
 #include "../headers/systems/mesh.h"
 #include "../headers/systems/render.h"
+#include "../headers/systems/transform.h"
+#include "../headers/systems/orthographic_camera.h"
 #include "../headers/factories/orthographic_camera.h"
 #include "../headers/factories/mesh.h"
 
@@ -16,7 +18,7 @@ auto main() -> int
   blossom::window window(WINDOW_WIDTH, WINDOW_HEIGHT, window_title);
   window.enter_fullscreen();
 
-  blossom::shader triangle_shader {"shaders/triangle.frag", "shaders/triangle.vert"};
+  blossom::shader default_shader {"shaders/default.frag", "shaders/default.vert"};
 
   const std::vector<glm::vec3> TRIANGLE_VERTICES =
   {
@@ -26,18 +28,26 @@ auto main() -> int
     { 0.5F,  -0.289F,  0.0F}
   };
 
+  const glm::vec3 TRIANGLE_SCALE {200.0F, 200.0F, 200.0F};
+
   entt::registry registry;
+
+  constexpr glm::vec3 CAMERA_POSITION = { 0.0F, 0.0F, 5.0F };
 
   blossom::factory::orthographic_camera{registry}
     .with_width  (WINDOW_WIDTH)
     .with_height (WINDOW_HEIGHT)
+    .with_position (CAMERA_POSITION)
     .make_active();
 
   blossom::factory::mesh(registry)
     .with_vertices(TRIANGLE_VERTICES)
-    .with_shader_program(triangle_shader.program_id);
+    .with_scale(TRIANGLE_SCALE)
+    .with_shader_program(default_shader.program_id);
 
   blossom::system::mesh::init(registry);
+  blossom::system::transform::update(registry);
+  blossom::system::orthographic_camera::update(registry);
 
   while ( glfwWindowShouldClose(window.window_ptr) == 0 )
   {
