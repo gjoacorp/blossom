b8b6b47617b52b7bfe6733dc0dfe9e04c2ceb1ed
eugenekingfish <aojg@protonmail.ch>
2026-02-08 01:01:17 +0000

    feat: update factories::mesh - move old system::mesh logic into build fn

--- headers/factories/mesh.h
+++ headers/factories/mesh.h
@@ -30,15 +30,15 @@ namespace blossom::factory
         return *this;
       }
 
-      auto with_vertices(const std::vector<glm::vec3>& vertices) -> mesh&
+      auto with_vertices(std::vector<glm::vec3> vertices) -> mesh&
       {
-        registry_.get<component::mesh>(entity_).vertices = vertices;
+        vertices_ = std::move(vertices);
         return *this;
       }
 
-      auto with_indices(const std::vector<GLuint>& indices) -> mesh&
+      auto with_indices(std::vector<GLuint> indices) -> mesh&
       {
-        registry_.get<component::mesh>(entity_).indices = indices;
+        indices_ = std::move(indices);
         return *this;
       }
 
@@ -60,14 +60,80 @@ namespace blossom::factory
         return *this;
       }
 
+      auto clean_up() -> entt::entity
+      {
+        vertices_.clear(); indices_.clear();
+        vertices_.shrink_to_fit(); indices_.shrink_to_fit();
+
+        return entity_;
+      }
+
       auto build() -> entt::entity
       {
+        auto& mesh = registry_.get<component::mesh>(entity_);
+
+        mesh.vertex_count = static_cast<GLsizei>(vertices_.size());
+        mesh.index_count = static_cast<GLsizei>(indices_.size());
+
+        init_uniform_locations_();
+        init_buffers_();
+
         return entity_;
       }
 
     private:
-      entt::registry&       registry_;
-      entt::entity          entity_;
+      entt::registry&        registry_;
+      entt::entity           entity_;
+
+      std::vector<glm::vec3> vertices_;
+      std::vector<GLuint> indices_;
+
+      void init_uniform_locations_()
+      {
+        auto& mesh = registry_.get<component::mesh>(entity_);
+
+        mesh.model_uniform_location      = glGetUniformLocation(mesh.shader_program, "model");
+        mesh.view_uniform_location       = glGetUniformLocation(mesh.shader_program, "view");
+        mesh.projection_uniform_location = glGetUniformLocation(mesh.shader_program, "projection");
+      }
+
+      void init_buffers_()
+      {
+        auto& mesh = registry_.get<component::mesh>(entity_);
+
+        glCreateVertexArrays(1, &mesh.vao);
+        glBindVertexArray(mesh.vao);
+
+        glCreateBuffers(1, &mesh.vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, mesh.vbo);
+        glNamedBufferStorage(
+            mesh.vbo, 
+            static_cast<GLsizeiptr>( vertices_.size() * sizeof(glm::vec3) ), 
+            vertices_.data(), 
+            0);
+
+        if (indices_.size() > 0)
+        {
+          glCreateBuffers(1, &mesh.ebo);
+          glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.ebo);
+          glNamedBufferStorage(
+              mesh.ebo, 
+              static_cast<GLsizeiptr>( indices_.size() * sizeof(GLuint) ), 
+              indices_.data(), 
+              0);
+        }
+
+        glVertexAttribPointer(
+            0, 
+            3, 
+            GL_FLOAT, 
+            GL_FALSE, 
+            sizeof(glm::vec3), 
+            nullptr );
+
+        glEnableVertexAttribArray(0);
+        glBindVertexArray(0);
+      }
   };
 }
 
