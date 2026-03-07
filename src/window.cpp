62ef5985117ba88b75f8a2f708a777922c15ac23
sonstgenannt <118530645+sonstgenannt@users.noreply.github.com>
2026-03-07 12:26:24 +0000

    Wayland detection corrected and hint resolved.

--- src/window.cpp
+++ src/window.cpp
@@ -12,11 +12,23 @@ window::window(int width, int height, const char* title)
     throw std::runtime_error("ERROR: Failed to initialise GLFW.");
   }
 
+  int platform = glfwGetPlatform();
+  std::cout << platform << std::endl;
+
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
+
+  if (platform == GLFW_PLATFORM_WAYLAND) {
+      std::cout << "Wayland detected by GLFW." << std::endl;
+      glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
+  }
 
   window_ptr = glfwCreateWindow(width, height, title, NULL, NULL);
+
+  if (window_ptr == NULL ) {
+      std::cout << "Failed to create GLFW window." << std::endl;
+      glfwTerminate();
+  }
 
   glfwMakeContextCurrent(window_ptr);
   glfwSetFramebufferSizeCallback(window_ptr, framebuffer_size_callback_);
@@ -26,16 +38,9 @@ window::window(int width, int height, const char* title)
   GLenum err = glewInit();
   if ( err != GLEW_OK )
   {
-    std::string error_str = reinterpret_cast<const char*>(glewGetErrorString(err));
-    if (error_str == "No GLX display")
-    {
-      std::cout << "Wayland detected: Ignoring GLX init error. Continuing..." << "\n";
-    }
-    else
-    {
+      std::string error_str = reinterpret_cast<const char*>(glewGetErrorString(err));
       glfwTerminate();
       throw std::runtime_error("ERROR: Failed to initialise GLEW. Reason: " + error_str);
-    }
   }
 
   glfwSwapInterval(1); 
