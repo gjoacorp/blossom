fd794968109f583ba5a275b0b299131defa3245d
eugenekingfish <aojg@protonmail.ch>
2026-03-15 00:34:37 +0000

    refactor: rename dirty -> dirty_transform

--- headers/components/tags/dirty.h
+++
@@ -1,9 +0,0 @@
-#ifndef BLOSSOM_COMPONENT_TAG_DIRTY_H
-#define BLOSSOM_COMPONENT_TAG_DIRTY_H
-
-namespace blossom::component::tag
-{
-  struct dirty {};
-};
-
-#endif 
---
+++ headers/components/tags/dirty_transform.h
@@ -0,0 +1,9 @@
+#ifndef BLOSSOM_COMPONENT_TAG_DIRTY_TRANSFORM_H
+#define BLOSSOM_COMPONENT_TAG_DIRTY_TRANSFORM_H
+
+namespace blossom::component::tag
+{
+  struct dirty_transform {};
+};
+
+#endif 
